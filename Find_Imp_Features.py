38dc1e654b1433e0f1dab927c0894e3cb7fb91e1
can99sahin@gmail.com <can99sahin@gmail.com>
2022-12-04 20:51:01 +0300

    Random Forest on iris dataset

---
+++ Find_Imp_Features.py
@@ -0,0 +1,42 @@
+from sklearn import datasets
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+#Import train_test_split function
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Load dataset
+iris = datasets.load_iris()
+
+data = pd.DataFrame({
+    'sepal length':iris.data[:,0],
+    'sepal width':iris.data[:,1],
+    'petal length':iris.data[:,2],
+    'petal width':iris.data[:,3],
+    'species':iris.target
+})
+
+X = data[['sepal length', 'sepal width', 'petal length', 'petal width']]  # Features
+y = data['species']  # Labels
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+#Create a Gaussian Classifier
+clf = RandomForestClassifier(n_estimators=100)
+
+#Train the model using the training sets y_pred =clf.predict(X_test)
+clf.fit(X_train, y_train)
+
+feature_imp = pd.Series(clf.feature_importances_, index=iris.feature_names).sort_values(ascending=False)
+feature_imp
+
+# Creating a bar plot
+sns.barplot(x=feature_imp, y=feature_imp.index)
+
+# Add labels to your graph
+plt.xlabel('Feature Importance Score')
+plt.ylabel('Features')
+plt.title("Visualizing Important Features")
+plt.legend()
+plt.show()
